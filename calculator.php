c512906cb6f540ac8fdb0415b7261025ac5ed9a1
Jogis <ronipihajoki@proton.me>
2026-02-02 05:08:51 +0200

    Start of the sheet
    This is based on Dani Krossings
    PHP Tutorial for beginners.
    
    LINK:
    https://www.youtube.com/watch?v=l4_Vn-sTBL8&list=WL&index=8
    
    First commit:
    -Calculator
    -Main.css
    -Index.php
    
    (this is 2:40:17 into the video)

---
+++ calculator.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html> <!-- -->
+<head>
+    <title>CALCULATOR</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+<!-- We need to set action so a user can do something. We also need to set method so we can choose from GET or POST. (Get goes to URL) ((POST hides the data))-->
+<!-- We use $_SERVER["PHP_SELF"] so the data is sent back to the same page. (PHP self means the php file.)-->
+<!--echo htmlspecialchars lets us not get injected with code lol-->
+        <input type="number" name="num01" placeholder="Number one">
+<!-- We make sure that the user will type number. We name the attribute. Also give it a placeholder so the user knows what to type.-->
+<!-- The placeholder should make sense-->
+
+        <select name="operator"> <!--Way for us to create a dropdown inside of a form.-->
+            <option value="sum">+</option> <!--Adds a dropdown menu option for sum -->
+            <option value="subtract">-</option> <!--Adds a dropdown menu option for subtract -->
+            <option value="multiply">*</option> <!--Adds a dropdown menu option for multiply -->
+            <option value="divide">/</option> <!--Adds a dropdown menu option for divide -->
+        </select>
+
+        <input type="number" name="num02" placeholder="Number two"> <!--Lets us select a second number -->
+        
+        <button>Calculate</button> <!-- Adds a button -->
+    </form>
+
+    <?php
+
+
+    // These GRAB our data from inputs.
+    if($_SERVER["REQUEST_METHOD"] == "POST") { // We recceive the data with POST. And only run the if statement if there is data.
+        // filter_input lets us filter our data to not get injected with sql or java.
+        // FILTER_SANITIZE_NUMBER lets us pick ether _FLOAT or _INT
+        $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT); // We sanitize the data for num01.
+
+        $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT); // We sanitize the data for num02.
+
+        $operator = htmlspecialchars($_POST["operator"]); // FILTER_INPUT does not support Strings anymore, so we use htmlspecialchars
+
+
+        // These handle our Errors
+        // First we create a boolean to and it tells us if there is errors.
+        $errors = false;
+
+        if (empty($num01) || empty($num02) || empty($operator)){ // We check if the values are empty.
+            echo "<p class='error'>Fill in all fields!</p>"; // If any of the values is empty we print a reminder to fill the fields.
+            $errors = true; // We set errors as true.
+        }
+
+        if (!is_numeric($num01) || !is_numeric($num02)) { // We check if the return data IS NOT a number. ( ! )
+            echo "<p class='error'>Fill the fields with NUMBERS</p>"; // If any value is NOT a numberm we print this.
+            $errors = true; // We set errors as true.
+        }
+
+        // Calculate the numbers if no errors
+
+        if(!$errors){ // We check if there is no error messages. so we use ! in front.
+        
+            $value = 0; // We define a default value for the number, so we can actually use it with our result echo at line 80.
+
+            switch($operator) { // Now we actually check what operator is chosen and we calculate the numbers.
+                case "sum": // We get this from the select we created.
+                    $value = $num01 + $num02;
+                    break;
+                case "subtract": // We get this from the select we created.
+                    $value = $num01 - $num02;
+                    break;
+                case "multiply": // We get this from the select we created.
+                    $value = $num01 * $num02;
+                    break;
+                case "divide": // We get this from the select we created.
+                    $value = $num01 / $num02;
+                    break;
+                default:
+                    echo "Error";
+            }
+
+            echo "<p class='lasku'>Result = " . $value . "</p>";
+        }
+    }
+
+
+    ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<head>
+    <title>Cheat Sheet</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <main>
+        <?php
+// When we define a variable it does not need to be $string, it does not matter what the name is.
+// Do note variable names are case sensitive.
+// $test and $Test are too different variables.
+// But for example echo can be written: eCho, ECho, ECHO, echO
+
+// User defined VARIABLES
+
+    // Define string with " " | Null value = " "
+    $string = "Hei";
+
+    // Define int with a single number. | Null value = 0;
+    $int = 1;
+
+    // Define boolean with true or false, also we can use 1 & 0. | Null value = false/0.
+    $bool = true;
+
+    // Define float with decimal number. | Null value = 0;
+    $float = 0.12;
+
+    // Define Array with $name = array(). | Null value = [ ]; | We can add different data types inside of array.
+    $testiarray = array("testi1", "testi2", "testi2");
+
+    // Define a object $name = new classname(). | Null value = null;
+    $object = new testi();
+
+    // Define class
+    class testi{
+    
+    }
+
+
+// Pre-Defined VARIABLES "Superglobals"
+echo "<h2><b>Supergloblas</b></h2>";
+
+    // $_SERVER; holds information about the web server including headers, paths, and script locations.
+    // EXAMPLE OF $_SERVER
+    echo $_SERVER["PHP_SELF"]; // Prints path to the PHP file
+    echo "<br>";
+    echo $_SERVER["DOCUMENT_ROOT"]; // Prints the root to the file
+    echo "<br>";
+    echo $_SERVER["SERVER_NAME"]; // Prints the server name
+    echo "<br>";
+    echo $_SERVER["REQUEST_METHOD"];
+    echo "<br>";
+
+    // $_SESSION;
+    // EXAMPLE OF $_SESSION
+    $_SESSION["username"] = "Pihajoki"; // We set username as "Pihajoki"
+    echo $_SESSION["username"]; // We print username
+    echo "<br>";
+
+    // $_COOKIE;
+    // EXAMPLE OF $_COOKIE
+
+    // $_ENV;
+    // EXAMPLE OF $_ENV
+
+    // $_GET; contains an array of variables received via the HTTP GET method.
+    // EXAMPLE OF $_GET
+
+    // $_POST;  contains an array of variables received via the HTTP POST method.
+    // EXAMPLE OF $_POST
+
+    // $_FILES;
+    // EXAMPLE OF $_FILES
+
+    // $_REQUEST contains data from submitted forms, URL query strings, and HTTP cookie
+    // EXAMPLE OF $_REQUEST
+
+
+
+
+// This is how we handle form data.
+    // without htmlspecialchars we risk the possibility of code injection through forms.
+    if($_SERVER["REQUEST_METHOD"] == "$_POST"){ // We check if the request method of the server is post. if it is we follow with the following:
+        $firstname = htmlspecialchars($_POST["firstname"]); // We store the data from POST to variable firstname
+        $lastname = htmlspecialchars($_POST["lastname"]); // We store the data from POST to variable lastname
+
+        if(empty($firstname)){ // Empty checks if the user fills the value for firstname.
+            exit("Locationl ../index.php"); // If the user does not fill the value, user will be sent back to the home page.
+        }
+
+    // We can print data from the one we did above
+        echo "These are the data, POST submitted for us";
+        echo "<br>";
+        echo $firstname;
+        echo "<br>";
+        echo $lastname;
+
+        header("Location: ../index.php"); // Sends the user back.
+    } else {
+        header("Locationl ../index.php");
+    }
+
+
+// OPERATORS IN PHP
+echo "<br>";
+echo "<h2><b>Operators in PHP</b></h2>";
+
+    // String Operator
+    echo "<h3><b>String Operator</b></h3>";
+
+    $a = "Hello"; // Define string a
+    $b = "World!"; // Define string b
+    $c = $a . " " . $b; // . works as a + in Java. We can connect Strings & spaces.
+    echo $c; // Prints: "Hello World!"
+    echo "<br>";
+
+    // Arithmetic Operator
+    echo "<h3><b>Arithmetic Operators</b></h3>";
+
+    // We can eco calculations straight up.
+    echo 10 * 2; // Multiply
+    echo "<br>";
+    echo 10 + 2; // Sum
+    echo "<br>";
+    echo 10 - 2; // Substract
+    echo "<br>";
+    echo 10 / 2; // Divide
+    echo "<br>";
+    echo 10 % 5; // Modulus (10/3=9, 10-9=1.)
+    echo "<br>";
+    echo 10 ** 3; // Power of. (10 * 10 * 10)
+    echo "<br>";
+    echo 1 + 2 * 4; // The * is ccalculated first.
+    echo "<br>";
+    echo (1 + 2) * 4; // The sum inside of () will be calculated first.
+    echo "<br>";
+
+    // Assignment operators
+    echo "<h3><b>Assignment Operators</b></h3>";
+
+
+    $numero1 = 2; // Sign 2 as the value of numero1.
+    $numero1 += 4; // $numero1 = numero1 + 4; (These are the same. The final sum is 6.)
+    // This could be +=. *=, /=, %=, -=, **=.
+    echo $numero1;
+    echo "<br>";
+
+    // Comparison operator
+    echo "<h3><b>Comparison operator</b></h3>";
+
+    $numero2 = 2; // define int.
+    $numero3 = 4; // define int.
+    $numero4 = "2"; // define string.
+    $numero5 = 2;
+    $numero6 = 6;
+
+
+    if ($numero2 == $numero3) { // We compere if numero2 and numero3 are valued equally with ==
+        echo "Numbers are the same.";
+        echo "<br>";
+    } else {
+        echo "Numbers differ.";
+        echo "<br>";
+    }
+
+    if ($numero2 === $numero4) { // We compere if the numbers are equal AND the third = comperes if they are the same datatype. ===
+        echo "Values are the same & share the same type";
+        echo "<br>";
+    } else {
+        echo "Values dont share a type.";
+        echo "<br>";
+    }
+
+    if ($numero2 != $numero3) { // We check that the numbers are not EQUAL.
+        echo "Numbers are not equal.";
+        echo "<br>";
+    } else {
+        echo "Numbers are equal.";
+        echo "<br>";
+    }
+
+
+    // Logical Operators
+    echo "<h3><b>Logical Operators</b></h3>";
+    // "or" is the same as ||
+    // "and" is the same as &&
+
+    if ($numero2 == $numero5 && $numero3 == $numero6) { //  The first section with $n2 & $n5 are equal so its true. Second one $n3 & $n6 are not equal.
+    // "and" operator in the middle needs BOTH statements to be true. So this will not print anything.
+        echo "Numbers are the same.";
+        echo "<br>";
+    }
+
+    if ($numero2 == $numero5 || $numero3 == $numero6) { //  The first section with $n2 & $n5 are equal so its true. Second one $n3 & $n6 are not equal.
+    // "or" operator in the middle needs ONE statement to be true. So this will print "Numbers are the same".
+        echo "Numbers are the same.";
+        echo "<br>";
+    }
+
+        //   THIS IS TRUE         THIS IS FALSE           THIS IS FALSE
+    if ($numero2 == $numero5 || $numero3 == $numero6 && $numero2 == $numero6) {
+    // We can have as many of these operators we want.
+    // This will print the "Numbers are the same."
+    // Even tho the right side of || is coplitely wrong.
+    // In theory, my code is suposed to have both of them "correct"
+        echo "Numbers are the same.";
+        echo "<br>";
+    }
+
+            //   THIS IS TRUE         THIS IS FALSE           THIS IS FALSE
+    if ($numero2 == $numero5 || $numero3 == $numero6 && $numero2 == $numero6) {
+    // We can have as many of these operators we want.
+    // This will print the "Numbers are the same."
+    // Even tho the right side of || is coplitely wrong.
+    // In theory, my code is suposed to have both of them "correct"
+        echo "Numbers are the same.";
+        echo "<br>";
+    }
+
+
+
+    // Incrementing/Decrementing Operators
+    echo "<h3><b>Incrementing/Decrementing Operators</b></h3>";
+
+    $increment = 1;
+    echo ++$increment; // Prints 2. ++ will add 1 to the default value.
+    echo "<br>";
+
+    $decrement = 2;
+    echo --$decrement; // Prints 1. -- will substract from the default value.
+    echo "<br>";
+
+    echo $increment++; // prints 2. adds 1 after its printed (Prints 2 due to line 249 ++)
+    echo $increment; // Prints 3. due to line above.
+
+
+
+
+// Control Structures in PHP
+echo "<br>";
+echo "<h2><b>Control structures in PHP</b></h2>";
+
+    $bool1 = true;
+    $int1 = 1;
+    $int2 = 4;
+    $nimi = "Roni";
+
+    if (!$bool1) { // ! in front of booleam is checking if the condition is false.
+        echo "its false.";
+        echo "<br>";
+    }
+
+
+
+    // Wrong way.
+    if ($int1 < $int2) { // < lesser than && > more than.
+        echo "WRONG WAY: First condition is true!";
+        echo "<br>";
+
+            if ($int1 > $int2) { // THIS IS NESTING. IT MAKES CODE MESSY. DONT DO IT. (So if, inside of if...etc)
+                echo "WRONG WAY: Second condition is true!";
+                echo "<br>";
+            }
+    }
+
+    // Right way.
+    if ($int1 < $int2 && !$bool1) { // First it checks if our first condidion is true. Then && we add our boolean. It will check if its false. (It's true.) So this wont print.
+        echo "Right way: First condition is true!";
+        echo "<br>";
+    } else if ($int1 < $int2 && $bool1) { // Same as previous, but checks if boolean is true. !THIS IS CORRECT WAY!
+        echo "Right way: Second condition is true!";
+        echo "<br>";
+    } else { // This is added just so we know if both are wrong. Known as "Default code"
+        echo "None of the conditions were true!";
+        echo "<br>";
+    }
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // To avoid nesting, we use else if () {
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    // CASE
+    // We use switch to check single value. we use if statements for multiple value.
+    switch ($nimi){ // we define the value we want to check inside of switch. This block of code goes to different directions, depending on certain result.
+        case "Roni": // case VALUE: Value = the value we are comparing  $nimi to.
+            echo "The name is correct!";
+            echo "<br>";
+            break; // we need to stop the case with break.
+
+        case "Dimi": // Here its incorrect so this wont print.
+            echo "The name is correct!";
+            echo "<br>";
+            break;
+        
+        default: // Default reply if cases are all incorrect.
+            echo "None of the names are correct.";
+    }
+
+
+
+
+    // MATCH
+    $result = match ($nimi) { // Inside of match(), we place the variable we want to check.
+        "Roni" => "Name is Roni", // => This changes the value of $nimi to "The name is Roni" WE ALWAYS END MATCH IN , EVEN IF ITS THE LAST ONE.
+        "Dimi", "Viitakoski" => "Name is Dimi", // We can also have multiple values we compere, and if any are correct we change the value.
+        default => "Non of the names match.",
+    }; // MATCH NEEDS TO END ON A ; THIS IS SAME AS CREATING A VARIABLE.
+
+
+        echo $result;
+
+
+
+        ?>
+    </main>
+</body>
+
+</html>
